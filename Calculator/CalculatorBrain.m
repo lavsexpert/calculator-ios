1ad2ecb619950ff2624e0c9a5edf6ad2368a2a89
Лавров Сергей <lavrov-sergey@yandex.ru>
2019-05-26 00:55:15 +0300

    Рефакторинг: Добавлены комментарии и упрощен код

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -19,50 +19,50 @@
     self = [super init];
     if (self){
         currents = 0;
-        newDigit = false;
+        newDigit = true;
         operation = @"";
     }
     return self;
 }
 
+// Добавление цифр и десятичной точки
 - (NSString*)addDigit :(NSString *)button :(NSString *)display{
+    // Когда начинаем набирать, то пишем нажатую кнопку или "0." если нажали "." и отмечаем, что продолжаем набор
     if(newDigit){
         display = ([button isEqualToString:@"."]) ? @"0." : button;
         newDigit = false;
+    // Если продолжаем набирать и нажата кнопка "." - то добавляем только если нет точки в числе
     } else if ([button isEqualToString:@"."]) {
         if([display rangeOfString:@"."].location == NSNotFound){
             display = [display stringByAppendingString:@"."];
         }
+    // Продолжаем набирать цифры, но не более 10 - чтобы не вылезти за пределы экрана
     } else {
-        if([display isEqualToString: @"0"]){
-            display = button;
-        } else if(display.length < 10) {
+        if(display.length < 10) {
             display = [display stringByAppendingString: button];
         }
     }
     return display;
 }
 
+// Выполнение операций над числами
 - (NSString*)compute :(NSString *)button :(NSString *)display{
+    // Очистка операций с числами
     if ([button isEqualToString:@"C"]) {
         currents = 0;
         operation = @"";
         display = @"0";
-        newDigit = true;
+    // Изменение знака числа
     } else if([button isEqualToString:@"+/-"]){
         display = [NSString stringWithFormat:@"%f", [display doubleValue] * (-1)];
-        newDigit = true;
+    // Получение обратного числа с проверкой на 0 - для него возвращается 0/
     } else if([button isEqualToString:@"1/x"]){
-        if([display doubleValue] == 0){
-            display = @"0";
-        } else {
-            display = [NSString stringWithFormat:@"%f", 1 / [display doubleValue]];
-        }
-        newDigit = true;
+        display = ([display doubleValue] == 0) ? @"0" : [NSString stringWithFormat:@"%f", 1 / [display doubleValue]];
+    // Если операция уже была выполнена - то просто выводим результат
     } else if(newDigit && ![operation isEqualToString:@"="]){
         display = [NSString stringWithFormat:@"%f", currents];
+    // А теперь бинарные операции + - * / %
     } else {
-        newDigit = true;
         if ([operation isEqualToString:@"+"]) {
             currents += [display doubleValue];
         }
@@ -73,15 +73,10 @@
             currents *= [display doubleValue];
         }
         else if ([operation isEqualToString:@"/"]){
-            if ([display doubleValue] == 0){
-                currents = 0;
-            } else {
-                currents /= [display doubleValue];
-            }
+            currents = ([display doubleValue] == 0) ? 0 : currents / [display doubleValue];
         }
         else if ([operation isEqualToString:@"%"]){
             currents = ([display doubleValue] / 100) * currents;
-            newDigit = true;
         }
         else {
             currents = [display doubleValue];
@@ -89,6 +84,7 @@
         display = [NSString stringWithFormat:@"%f", currents];
         operation = button;
     }
+    newDigit = true;
     return display;
 }
 
